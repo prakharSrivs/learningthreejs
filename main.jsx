de845f3a71a25efa2f4b364e2fa8bf60f4291881
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-08-14 01:18:36 +0530

    Orbits, Cameras, Rotations, Animations

--- main.jsx
+++ main.jsx
@@ -18,18 +18,38 @@ scene.add(camera);
 camera.position.setZ(5);
 
 const canvas = document.querySelector("#threejs");
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true
 controls.autoRotate = true;
 
-const renderLoop = ()=>{
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const renderLoop = ()=>{    
+    cubeMesh.rotation.y += THREE.MathUtils.degToRad(10);
+    let currentTime = clock.getElapsedTime();
+    let delta = currentTime - previousTime;
+    cubeMaterial.color.r = Math.sin(currentTime);
+    cubeMaterial.color.g = 1 - Math.sin(currentTime);
+    cubeMaterial.color.b = 1 - Math.sin(currentTime);
+    previousTime = currentTime;
+    cubeMesh.position.x = Math.sin(currentTime)*2;
+    cubeMesh.scale.x = Math.sin(currentTime)*2;
     controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(renderLoop);
 }
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ 
+    canvas,
+    antialias: true
+});
+const maxPixelRatio = Math.min(2, window.devicePixelRatio);
+
+renderer.setPixelRatio(maxPixelRatio);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
